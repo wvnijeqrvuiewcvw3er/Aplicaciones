65c08d3cd1be081613625d1223c2cede892e5356
Marcos Alonso Chable <marcos26alonso@gmail.com>
2023-07-12 09:12:23 -0600

    Cambio

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,17 +1,65 @@
+"use client"
+import React from "react"
 import "./login.css"
+import { register } from "../services/firebase/auth";
 
-export default function Login() {
+export default function Register(email, password, displayName) {
+
+    const[state,setState] = React.useState({
+        email:"",
+        password:"",
+        displayName:""
+    });
+
+    function onChange(event) {
+        
+        const value = event.target.value;
+        const name = event.target.name;
+        
+        setState({
+            ...state, 
+            [name]: value
+        })
+    
+        
+    }
+
+    function onRegister(event) {
+        //const email = state.email;
+        //const password = state.password;
+        //const displayName = state.displayName;
+        event.preventDefault();
+
+        const args ={email, password, displayName}
+
+        register(args);
+    }
 
     return (
-        <div className="container">
-            <input 
-            placeholder="email" 
-            type="text" />
-            <input 
-            placeholder="123456" 
-            type="password" />
-            <button>Login</button>
-        </div>
-    )
+     <div >
+       <form onSubmit={onRegister} className="container">
+            <input name="email" onChange={onChange} value={state.email} type="text" placeholder="Correo"/> <br></br>
 
-}
+            <input name="password" 
+            onChange={onChange} 
+            value={state.password} 
+            type="password" 
+            placeholder="Password"
+            required
+            /> <br></br>
+
+            <input name="displayName"
+                onChange={onChange}
+                value={state.displayName}
+                placeholder="Tu Nombre"
+                type="text"
+                required
+            /> <br></br>
+ 
+            <button action="submit">Enviar Formulario</button>
+        </form>
+     </div>
+    )
+  }
+
+
--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: "AIzaSyCLhXk8wRQ8Q3N1Okwdd3hakI5sac26fr8",
+  authDomain: "aplicaciones-3c.firebaseapp.com",
+  projectId: "aplicaciones-3c",
+  storageBucket: "aplicaciones-3c.appspot.com",
+  messagingSenderId: "570539153980",
+  appId: "1:570539153980:web:3ba3f2acba211f3078c139",
+  measurementId: "G-ZJVED20V8L"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+const analytics = getAnalytics(app);
